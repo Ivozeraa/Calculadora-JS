57d0b8f8961163c495e2126879119877aae8fb51
Ivozeraa <joseivo2008@gmail.com>
2025-03-13 15:55:28 -0300

    Calculadora

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 var visor = document.getElementById("visor");
 
 function insValor(valor) {
-    visor.value += valor;
+  visor.value += valor;
 }
 
 function limparVisor() {
-    visor.value = null;
+  visor.value = "";
 }
 
 function calcResultado() {
-    try {
-        visor.value = eval(visor.value);
-    } catch {
-        alert("ERRO: Cálculo inválido.");
-        visor.value = null;
+  try {
+    if (visor.value.trim() === "") {
+      visor.value = "";
+      return;
     }
+
+    const resultado = eval(visor.value);
+    
+    if (resultado === undefined || resultado === null || isNaN(resultado)) {
+      visor.value = "";
+    } else {
+      visor.value = resultado;
+    }
+  } catch {
+    alert("ERRO: Cálculo inválido.");
+    visor.value = "";
+  }
 }
 
-document.addEventListener('keydown', function(event) {
-    const key = event.key;
 
-    if ((key >= '0' && key <= '9') || ['+', '-', '*', '/'].includes(key)) {
-        insValor(key);
-    }
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
 
-    if (key === 'Enter' || key === "=") {
-        calcResultado();
-    }
+  switch (true) {
+    case key >= "0" && key <= "9":
 
-    if (key === 'Backspace') {
-        visor.value = visor.value.slice(0, -1);
-    }
-
-    if (key === 'Escape') {
-        limparVisor();
-    }
+    case ["+", "-", "*", "/"].includes(key):
+      insValor(key);
+      break;
+    case key === "Enter" || key === "=":
+      calcResultado();
+      break;
+    case key === "Backspace":
+      visor.value = visor.value.slice(0, -1);
+      break;
+    case key === "Escape":
+      limparVisor();
+      break;
+  }
 });
+
+
